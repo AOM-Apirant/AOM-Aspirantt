f91fc25d18237232739e390118402aa3e1f1e069
AOM-Aspirant <aomaspirant@gmail.com>
2025-07-13 02:37:22 +0530

    assets pdf updated

--- assets/pdf.ts
+++ assets/pdf.ts
@@ -44,7 +44,7 @@ export const gsrpdfFiles = [
     description: "Amendment Slip 17 to SCR G&SR 2020",
     icon: FileText,
   },
-  { 
+  {
     name: "AS 16 to G&SR 2020",
     filename: "AS 16 to G&SR 2020.pdf",
     size: "803KB",
@@ -156,7 +156,6 @@ export const gsrpdfFiles = [
     description: "Amendment Slip 1 to SCR G&SR 2020",
     icon: FileText,
   },
-  
 ];
 
 export const accidentPdfFiles = [
@@ -197,5 +196,40 @@ export const accidentPdfFiles = [
     size: "116KB",
     description: "Accident Manual Amendment Slip 2 - Safety protocol updates",
     icon: FileText,
+  },
+];
+
+export const pdfFiles = [
+  {
+    id: 1,
+    title: "Digital G&SR 2020 Upto AS 21",
+    description: "Digital G&SR 2020 Upto Amendment Slip 21",
+    filename: "/pdfs/Digital SCR G&SR 2020 upto AS 21.pdf",
+    size: "12.9 MB",
+    category: "G & SR",
+  },
+  {
+    id: 2,
+    title: "AS 21 to G&SR 2020",
+    description: "Amendment Slip 21 to G&SR 2020",
+    filename: "/pdfs/AS 21 to SCR G&SR 2020.pdf",
+    size: "657 KB",
+    category: "G & SR",
+  },
+  {
+    id: 3,
+    title: "AS 20 to G&SR 2020",
+    description: "Amendment Slip 20 to G&SR 2020",
+    filename: "/pdfs/AS 20 to G&SR 2020.pdf",
+    size: "546 KB",
+    category: "G & SR",
+  },
+  {
+    id: 4,
+    title: "AS 19 to G&SR 2020",
+    description: "Amendment Slip 19 to G&SR 2020",
+    filename: "/pdfs/AS 19 to G&SR 2020.pdf",
+    size: "175 KB",
+    category: "G & SR",
   },
 ];
