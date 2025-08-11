4a9fbf9083b5aefd29b2fe6894b0b8e7efd78086
AOM-Aspirant <aomaspirant@gmail.com>
2025-08-11 10:43:44 +0530

    subject and date added

--- assets/circulars.ts
+++ assets/circulars.ts
@@ -2,50 +2,74 @@ export const alertAdvicePdfFiles = [
   {
     name: "Alert Advice 1 of 2025",
     filename: "Alert Advice 1 of 2025.pdf",
+    subject: "Shunting instructions & Precautions",
+    date: "15/01/2025"
   },
   {
     name: "Alert Advice 2 of 2025",
     filename: "Alert Advice 2 of 2025.pdf",
+    subject: "Accidents/Unusuauls - Human related factors",
+    date: "15/02/2025"
   },
   {
     name: "Alert Advice 3 of 2025",
     filename: "Alert Advice 3 of 2025.pdf",
+    subject: "Joint procedural order for Disconnection/Reconnection of S&T gears",
+    date: "05/03/2025"
   },
   {
     name: "Alert Advice 4 of 2025",
     filename: "Alert Advice 4 of 2025.pdf",
+    subject: "Amendment Slip No 14 of G&SR 2020 Pertaining to Appendix-XIII",
+    date: "17/03/2025"
   },
   {
     name: "Alert Advice 5 of 2025",
     filename: "Alert Advice 5 of 2025.pdf",
+    subject: "Engineering Officials - Summer Precautions",
+    date: "02/04/2025"
   },
   {
     name: "Alert Advice 6 of 2025",
     filename: "Alert Advice 6 of 2025.pdf",
+    subject: "Precautions to be taken when Lurch/Jerk/Any other unsual experienced in the Section",
+    date: "10/04/2025"
   },
   {
     name: "Alert Advice 7 of 2025",
     filename: "Alert Advice 7 of 2025.pdf",
+    subject: "Strict adherence of Rules/Instructions while passing of Intermediate Block Stop Signal at ON position",
+    date: "08/05/2025"
   },
   {
     name: "Alert Advice 8 of 2025",
     filename: "Alert Advice 8 of 2025.pdf",
+    subject: "Prevention of Fire Accidents - Precautions",
+    date: "16/05/2025"
   },
   {
     name: "Alert Advice 9 of 2025",
     filename: "Alert Advice 9 of 2025.pdf",
+    subject: "Monsoon Precautions",
+    date: "29/05/2025"
   },
   {
     name: "Alert Advice 10 of 2025",
     filename: "Alert Advice 10 of 2025.pdf",
+    subject: "Procedure during failure of Automatic Signalling",
+    date: "25/06/2025"
   },
   {
     name: "Alert Advice 11 of 2025",
     filename: "Alert Advice 11 of 2025.pdf",
+    subject: "Securing of stabled rolling stock and precautions",
+    date: "17/07/2025"
   },
   {
     name: "Alert Advice 12 of 2025",
     filename: "Alert Advice 12 of 2025.pdf",
+    subject: "Standardized telephone sentences for LC Gates",
+    date: "01/08/2025"
   },
 ];
 
@@ -53,46 +77,57 @@ export const flyLeafPdfFiles = [
   {
     name: "Fly Leaf 1 of 2025",
     filename: "Fly Leaf 1 of 2025.pdf",
+    subject: "Air Brake Rake Testing Procedure (RTR) - LHB Coaching Stock",
   },
   {
     name: "Fly Leaf 2 of 2025",
     filename: "Fly Leaf 2 of 2025.pdf",
+    subject: "Modified 32 - Point Action Plan to prevent SPAD",
   },
   {
     name: "Fly Leaf 3 of 2025",
     filename: "Fly Leaf 3 of 2025.pdf",
+    subject: "Revised joint procedure order for Loading & Unloading & Transporation of Ballast Departmental Material Trains (DMT)",
   },
   {
     name: "Fly Leaf 4 of 2025",
     filename: "Fly Leaf 4 of 2025.pdf",
+    subject: "Summer Precautions",
   },
   {
     name: "Fly Leaf 5 of 2025",
     filename: "Fly Leaf 5 of 2025.pdf",
+    subject: "Joint procedure order in connection with Machine packing oftrack around the Digital axle counters sensors in Automatic Block Section",
   },
   {
     name: "Fly Leaf 6 of 2025",
     filename: "Fly Leaf 6 of 2025.pdf",
+    subject: "Joint procedure order for Securing and Releasing of Vehicles/Load/Train/Loco in Station/Yard or in Block Section",
   },
   {
     name: "Fly Leaf 7 of 2025",
     filename: "Fly Leaf 7 of 2025.pdf",
+    subject: "Monsoon patrolling of Track",
   },
   {
     name: "Fly Leaf 8 of 2025",
     filename: "Fly Leaf 8 of 2025.pdf",
+    subject: "procedure for Checking capability of Air Brake Locomotives",
   },
   {
     name: "Fly Leaf 9 of 2025",
     filename: "Fly Leaf 9 of 2025.pdf",
+    subject: "Monsoon safety precautions at Construction worksites",
   },
   {
     name: "Fly Leaf 10 of 2025",
     filename: "Fly Leaf 10 of 2025.pdf",
+    subject: "Important instructions for Officers/Senior Supervisors in Accidents",
   },
   {
     name: "Fly Leaf 11 of 2025",
     filename: "Fly Leaf 11 of 2025.pdf",
+    subject: "Push Trolley/Lorry working",
   },
 ];
 
@@ -100,22 +135,32 @@ export const operatingSafetyCircularPdfFiles = [
   {
     name: "Operating Safety Circular 03 of 2025",
     filename: "Operating Safety Circular 03 of 2025.pdf",
+    subject: "Procedure to pass IBS at ON position in case of IB phone is out of order",
+    date: "13/05/2025"
   },
   {
     name: "Operating Safety Circular 04 of 2025",
     filename: "Operating Safety Circular 04 of 2025.pdf",
+    subject: "Not turning operating handle to TOL position in SGE block instrument",
+    date: "16/05/2025"
   },
   {
     name: "Operating Safety Circular 05 of 2025",
     filename: "Operating Safety Circular 05 of 2025.pdf",
+    subject: "Working of Station staff during adverse weather conditions and asset failure situations",
+    date: "22/05/2025"
   },
   {
     name: "Operating Safety Circular 06 of 2025",
     filename: "Operating Safety Circular 06 of 2025.pdf",
+    subject: "Monsoon Precautions",
+    date: "24/06/2025"
   },
   {
     name: "Operating Safety Circular 07 of 2025",
     filename: "Operating Safety Circular 07 of 2025.pdf",
+    subject: "Guidelines for Operation of Long Haul Trains",
+    date: "24/06/2025"
   },
 ];
 
@@ -123,10 +168,14 @@ export const operatingGeneralCircularPdfFiles = [
   {
     name: "Operating General Circular 01 of 2025",
     filename: "Operating General Circular 01 of 2025.pdf",
+    subject: "Upgraded Inspection Check list for Station Inspections",
+    date: "04/03/2025"
   },
   {
     name: "Operating General Circular 02 of 2025",
     filename: "Operating General Circular 02 of 2025.pdf",
+    subject: "Operation of EWN, EUYN and OYN for Cancellation",
+    date: "05/05/2025"
   },
 ];
 
@@ -244,30 +293,44 @@ export const safetyDrivePdfFiles = [
   {
     name: "Safety Drive July 1 2025",
     filename: "Safety Drive July 1 2025.pdf",
+    subject: "Railway Board Safety drive No:08 (FY:2025-26) - Special drive on SPAD covering all LPs, ALPs, Track Machine Operators and Tower Wagon Operators at all locations",
+    date: "03/07/2025"
   },
   {
     name: "Safety Drive July 2 2025",
     filename: "Safety Drive July 2 2025.pdf",
+    subject: "Railway Board Safety Calendar Drive for the month of July 2025",
+    date: "04/07/2025"
   },
   {
     name: "Safety Drive July 3 2025",
     filename: "Safety Drive July 3 2025.pdf",
+    subject: "Railway Board Safety drive No:09 Safety in securing of stabled rolling stock",
+    date: "05/07/2025"
   },
   {
     name: "Safety Drive July 4 2025",
     filename: "Safety Drive July 4 2025.pdf",
+    subject: "Railway Board Safety drive No:10 Safety Inspection of block section Level Crossing Gates for 15 days",
+    date: "09/07/2025"
   },
   {
     name: "Safety Drive July 5 2025",
     filename: "Safety Drive July 5 2025.pdf",
+    subject: "Divisional Safety Drive No:03 Prevention of Fire cases in Trains",
+    date: "17/07/2025"
   },
   {
     name: "Safety Drive August 1 2025",
     filename: "Safety Drive August 1 2025.pdf",
+    subject: "Railway Board Safety drive No:11 Safety inspection of all Brake Vans of Passenger carrying trins to check about safety items",
+    date: "01/08/2025"
   },
   {
     name: "Safety Drive August 2 2025",
     filename: "Safety Drive August 2 2025.pdf",
+    subject: "Divisional Safety drive No:04 - Inspection of Emergency Crossovers & Rusty rails",
+    date: "04/08/2025"
   },
 ];
 
@@ -275,9 +338,13 @@ export const ruleOfTheMonthPdfFiles = [
   {
     name: "Rule of the Month PDF File for July August & September 2025",
     filename: "Rule of the Month Jul Aug Sep 2025.pdf",
+    subject: "Rules of the Month for Monthly Safety Meetings",
+    date: "24/06/2025"
   },
   {
     name: "Rule of the Month - August 2025",
     filename: "Alert Advice 10 of 2025.pdf",
+    subject: "Procedure during failure of Automatic Signalling",
+    date: "25/06/2025"
   },
 ];
