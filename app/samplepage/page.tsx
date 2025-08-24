28793216d82af40acfe1e728c53a6f87beb3e8d2
AOM-Aspirant <aomaspirant@gmail.com>
2025-08-24 20:28:12 +0530

    OMPAGE56 ADDED

--- app/samplepage/page.tsx
+++ app/samplepage/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import OMPage42 from '@/components/optg-manual/optg-manual-index-pages/OMPage42'
+import OMPage56 from '@/components/optg-manual/optg-manual-index-pages/OMPage56'
+
 
 const SamplePage = () => {
   return (
     <div>
         <h1>Sample Page</h1>
-        <OMPage42/>
+        <OMPage56/>
     </div>
   )
 }
