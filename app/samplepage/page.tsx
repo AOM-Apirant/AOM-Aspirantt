274ead379d492bb3f4817e918f4120fa4fec8adb
AOM-Aspirant <aomaspirant@gmail.com>
2025-08-26 10:43:42 +0530

    OM PAGE 92 UPDATED NOW

--- app/samplepage/page.tsx
+++ app/samplepage/page.tsx
@@ -1,11 +1,11 @@
-import OMPage92 from '@/components/optg-manual/optg-manual-index-pages/OMPage92'
+import OMPage102 from '@/components/optg-manual/optg-manual-index-pages/OMPage102'
 import React from 'react'
 
 const SamplePage = () => {
   return (
     <div>
         <h1>Sample Page</h1>
-        <OMPage92/>
+        <OMPage102/>
         
     </div>
   )
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -317,7 +317,7 @@ export default function Navbar() {
               aria-label="Toggle menu"
             >
               <svg
-                className="w-5 h-5"
+                className="w-6 h-6"
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
