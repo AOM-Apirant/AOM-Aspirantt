9ec785f09c24d0aea0dc1d58fd503f629fadc67e
AOM-Aspirant <aomaspirant@gmail.com>
2025-09-01 23:25:35 +0530

    AMCHAPTERPAGE27B UPDATED

--- app/samplepage/page.tsx
+++ app/samplepage/page.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import AMChapterPage12 from '@/components/acc-manual/am-chapter-pages/AMChapterPage12'
+import AMChapterPage27B from '@/components/acc-manual/am-chapter-pages/AMChapterPage27B'
 
 const SamplePage = () => {
   return (
     <div>
         <h1>Sample Page</h1>
-        <AMChapterPage12/>
+        <AMChapterPage27B/>
         
     </div>
   )
 }
 
 export default SamplePage
+
+
+// And dont skip the content that I provided (cover all the content and dont add any extra content other than what I provided).
