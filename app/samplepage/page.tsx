963d1102a136d820af71223ebc7881278174e769
AOM-Aspirant <aomaspirant@gmail.com>
2025-09-04 12:50:56 +0530

    AMCHAPTERPAGE54 UPDATED

--- app/samplepage/page.tsx
+++ app/samplepage/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import AMChapterPage41B from '@/components/acc-manual/am-chapter-pages/AMChapterPage41B'
+import AMChapterPage54 from '@/components/acc-manual/am-chapter-pages/AMChapterPage54'
 const SamplePage = () => {
   return (
     <div>
         <h1>Sample Page</h1>
-        <AMChapterPage41B/>
+        <AMChapterPage54/>
 
     </div>
   )
@@ -14,3 +14,6 @@ export default SamplePage
 
 
 // And dont skip the content that I provided (cover all the content and dont add any extra content other than what I provided).
+
+
+// I added some more content to existing content to continue, make it beautiful too. And dont skip the content that I provided (cover all the content and dont add any extra content other than what I provided).
