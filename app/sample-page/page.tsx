7ec184037fa205403b0ddb81ce7969da84c3ecf5
AOM-Aspirant <aomaspirant@gmail.com>
2025-10-02 14:26:18 +0530

    BWMPARTAPAGE5B COMPLETED

--- app/sample-page/page.tsx
+++ app/sample-page/page.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
-import AMAppendixPage208B from '@/components/acc-manual/am-appendix-pages/AMAppendixPage208B'
+import BWMPartAPage5B from '@/components/bwm/bwm-part-a-pages/BWMPartAPage5B'
 const SamplePage = () => {
   return (
     <div>
         <h1>Sample Page</h1>  
-        <AMAppendixPage208B/>
+        <BWMPartAPage5B/>
     </div>
   )
 }
 
 export default SamplePage
+
+// Dont skip  or delete the content that I provided and dont add any extra content other than what I provided.
 
 
 // Make this content beautiful in the form of list style, in place of bullet points use simple icons related to content and content should be in english.
